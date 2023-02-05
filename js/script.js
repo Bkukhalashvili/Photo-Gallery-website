2ec3a902fa7cb270beefcb520d94224c25413fcf
Bkukhalashvili <bacho.kukhalashvili.1@gmail.com>
2023-02-05 18:55:06 +0400

    added footer

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@
 const infoContainerEl = document.querySelector(".info-container");
 const categoryEl = document.querySelector(".section-categories");
 const navBtn = document.querySelector(".main-nav-button");
+const footerEl = document.querySelector(".footer");
 
 navBtn.addEventListener("click", function () {
   infoContainerEl.classList.toggle("info-appear");
   categoryEl.classList.toggle("categories-slide-down");
+  footerEl.classList.toggle("footer-slide-down");
 });
+
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
