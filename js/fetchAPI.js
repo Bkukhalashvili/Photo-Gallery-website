efb351444d2d50bfe65c8506ee53c6472b1e92b7
Bkukhalashvili <bacho.kukhalashvili.1@gmail.com>
2023-02-16 21:17:58 +0400

    added dynamic category items  on index page

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,5 +1,9 @@
 const apiKey = config.API_KEY;
 
+// index page
+const categoriesGalleryEl = document.querySelector(".categories-gallery");
+
+// gallery page
 const photoGalleryEl = document.querySelector(".photo-gallery");
 const galleryPageTitle = document.querySelector("title");
 const headingEl = document.querySelector(".heading");
@@ -11,12 +15,12 @@ const baseUrl = document.URL;
 const searchQuery = baseUrl.slice(baseUrl.indexOf("?") + 1);
 // assign query parameter from url to gallery page title and heading
 galleryPageTitle.innerHTML += ` ${searchQuery}`;
-headingEl.innerHTML = searchQuery;
+if (headingEl) headingEl.innerHTML = searchQuery;
 
 // class name for galleryItemContainer
 let itemNum = 1;
-// creates elements and displays fetched data (images)
-function displayImages(response) {
+// creates elements and displays fetched data (images) on gallery page
+function displayGalleryImages(response) {
   response.photos.forEach((image) => {
     const galleryItemContainer = document.createElement("figure");
     galleryItemContainer.className = "gallery-item-container";
@@ -43,19 +47,97 @@ function displayImages(response) {
   });
 }
 
-// gets data
-async function SearchPhotos(query) {
-  const data = await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: apiKey,
-    },
-  });
-  const response = await data.json();
-  console.log(response);
+// class name for categoriesGalleryItemNum
+let CategoriesItemNum = 1;
+// creates elements and displays fetched data (images) on index page
+function displayCategoryImages(response, query) {
+  response.photos.forEach((image) => {
+    const categoriesGalleryItemContainer = document.createElement("figure");
+    categoriesGalleryItemContainer.className =
+      "categories-gallery-item-container";
+    categoriesGalleryItemContainer.className = `item${CategoriesItemNum}`;
 
-  displayImages(response);
+    const categoriesGalleryItemLink = document.createElement("a");
+    categoriesGalleryItemLink.className = "categories-gallery-item-link";
+    categoriesGalleryItemLink.href = `gallery.html?${query}`;
+
+    const categoriesGalleryItemTitle = document.createElement("p");
+    categoriesGalleryItemTitle.className = "categories-gallery-item-title";
+    categoriesGalleryItemTitle.innerHTML = query;
+
+    const categoriesGalleryItem = document.createElement("div");
+    categoriesGalleryItem.className = "categories-gallery-item";
+    categoriesGalleryItem.setAttribute(
+      "style",
+      `background-image: url(${image.src.large2x})`
+    );
+
+    categoriesGalleryItemLink.append(
+      categoriesGalleryItemTitle,
+      categoriesGalleryItem
+    );
+    categoriesGalleryItemContainer.append(categoriesGalleryItemLink);
+    categoriesGalleryEl.append(categoriesGalleryItemContainer);
+
+    CategoriesItemNum++;
+  });
 }
 
-SearchPhotos(searchQuery);
+// idex page = 0  gallery page = 1
+// if its 0 creates category list and fetches data for it
+// if its 1  fetches data for that searchQuery
+async function switcher(page) {
+  if (page === 0) {
+    // add new list item here to create new category
+    let list = [
+      "Japan",
+      "Norway",
+      "Germany",
+      "Italy",
+      "Greece",
+      "France",
+      "Turkey",
+      "Spain",
+      "Denmark",
+    ];
+    for (let i = 0; i < list.length; i++) {
+      await SearchPhotos(list[i], 1);
+    }
+  } else if (page === 1) {
+    SearchPhotos(searchQuery);
+  }
+}
+
+// gets data
+async function SearchPhotos(query, quantity) {
+  // quantity if images to fetch
+  if (quantity === 1) {
+    const data = await fetch(
+      `https://api.pexels.com/v1/search?query=${query}&per_page=${quantity}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: apiKey,
+        },
+      }
+    );
+    const response = await data.json();
+
+    displayCategoryImages(response, query);
+  } else {
+    const data = await fetch(
+      `https://api.pexels.com/v1/search?query=${query}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: apiKey,
+        },
+      }
+    );
+    const response = await data.json();
+
+    displayGalleryImages(response);
+  }
+}
