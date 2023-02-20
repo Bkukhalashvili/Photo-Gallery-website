eeb00fc1b1d218410632e257b07abd2953173bed
Bkukhalashvili <bacho.kukhalashvili.1@gmail.com>
2023-02-20 23:30:00 +0400

    made code more clean

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -19,83 +19,21 @@ if (headingEl) {
   galleryPageTitle.innerHTML += ` ${searchQuery}`;
   headingEl.innerHTML = searchQuery;
 }
-
-// class name for galleryItemContainer
+// counter for class name for galleryItemContainer and categoriesItemContainer
 let itemNum = 1;
-// creates elements and displays fetched data (images) on gallery page
-function displayGalleryImages(response) {
-  response.photos.forEach((image) => {
-    const galleryItemContainer = document.createElement("figure");
-    galleryItemContainer.className = "gallery-item-container";
-    galleryItemContainer.className = `item${itemNum}`;
 
-    const galleryItemLink = document.createElement("a");
-    galleryItemLink.className = "gallery-item-link";
-    galleryItemLink.href = image.url;
-    galleryItemLink.target = "_blank";
-
-    const galleryItem = document.createElement("div");
-    galleryItem.className = "gallery-item";
-    galleryItem.title = image.alt;
-    galleryItem.setAttribute(
-      "style",
-      `background-image: url(${image.src.large2x})`
-    );
-
-    galleryItemLink.append(galleryItem);
-    galleryItemContainer.append(galleryItemLink);
-    photoGalleryEl.append(galleryItemContainer);
-
-    itemNum++;
-  });
-}
-
-// list where fetched data for index page gets stored so it can be saved on local storage
+// list where fetched data for index page gets stored so it can be stored on local storage
 let categoriesData = [];
 
-// class name for categoriesGalleryItemNum
-let CategoriesItemNum = 1;
-// creates elements and displays fetched data (images) on index page
-function displayCategoryImages(response, query) {
-  response.photos.forEach((image) => {
-    const categoriesItemContainer = document.createElement("figure");
-    categoriesItemContainer.className = "categories-item-container";
-    categoriesItemContainer.className = `item${CategoriesItemNum}`;
-
-    const categoriesItemLink = document.createElement("a");
-    categoriesItemLink.className = "categories-item-link";
-    categoriesItemLink.href = `gallery.html?${query}`;
-
-    const categoriesItemTitle = document.createElement("p");
-    categoriesItemTitle.className = "categories-item-title";
-    categoriesItemTitle.innerHTML = query;
-
-    const categoriesItem = document.createElement("div");
-    categoriesItem.className = "categories-item";
-    categoriesItem.setAttribute(
-      "style",
-      `background-image: url(${image.src.large2x})`
-    );
-
-    categoriesItemLink.append(categoriesItemTitle, categoriesItem);
-    categoriesItemContainer.append(categoriesItemLink);
-    categoriesGalleryEl.append(categoriesItemContainer);
-
-    // adds fetched data to list
-    categoriesData.push(response);
-
-    CategoriesItemNum++;
-  });
-}
-
+// on load function
 // idex page = 0  gallery page = 1
-// if its 0 creates category list and fetches data for it
-// if its 1  fetches data for that searchQuery
+// if its 0 creates categories list and fetches data for it
+// if its 1  fetches data for that category(searchQuery)
 async function switcher(page) {
   // index page
   if (page === 0) {
     // add new list item here to create new category
-    let list = [
+    let categoriesList = [
       "Japan",
       "Norway",
       "Germany",
@@ -111,16 +49,21 @@ async function switcher(page) {
       localStorage.getItem("categoriesData")
     );
 
-    if (localCategoriesData && localCategoriesData.length === list.length) {
+    // checks if data exists on local storage and if its same as categories list
+    if (
+      localCategoriesData &&
+      localCategoriesData.length === categoriesList.length
+    ) {
       for (let i = 0; i < localCategoriesData.length; i++) {
-        displayCategoryImages(localCategoriesData[i], list[i]);
+        displayCategoryImages(localCategoriesData[i], categoriesList[i]);
       }
+      // if it doesn't exist or its not same as categories list, deletes it and creates a new one
     } else {
       localStorage.clear();
-      for (let i = 0; i < list.length; i++) {
-        await SearchPhotos(list[i], 1);
+      for (let i = 0; i < categoriesList.length; i++) {
+        await SearchPhotos(categoriesList[i], 1);
       }
-      // saves list(with fetched data for index page) to local storage
+      // stores list(with fetched data for index page) to local storage
       localStorage.setItem(`categoriesData`, JSON.stringify(categoriesData));
     }
     // gallery page
@@ -148,3 +91,64 @@ async function SearchPhotos(query, quantity) {
     ? displayCategoryImages(response, query)
     : displayGalleryImages(response);
 }
+
+// creates elements and displays fetched data (images) on index page
+const displayCategoryImages = function (response, query) {
+  response.photos.forEach((image) => {
+    const categoriesItemContainer = document.createElement("figure");
+    categoriesItemContainer.className = "categories-item-container";
+    categoriesItemContainer.className = `item${itemNum}`;
+
+    const categoriesItemLink = document.createElement("a");
+    categoriesItemLink.className = "categories-item-link";
+    categoriesItemLink.href = `gallery.html?${query}`;
+
+    const categoriesItemTitle = document.createElement("p");
+    categoriesItemTitle.className = "categories-item-title";
+    categoriesItemTitle.innerHTML = query;
+
+    const categoriesItem = document.createElement("div");
+    categoriesItem.className = "categories-item";
+    categoriesItem.setAttribute(
+      "style",
+      `background-image: url(${image.src.large2x})`
+    );
+
+    categoriesItemLink.append(categoriesItemTitle, categoriesItem);
+    categoriesItemContainer.append(categoriesItemLink);
+    categoriesGalleryEl.append(categoriesItemContainer);
+
+    // adds fetched data to fetched data list so it can be stored on local storage
+    categoriesData.push(response);
+
+    itemNum++;
+  });
+};
+
+// creates elements and displays fetched data (images) on gallery page
+const displayGalleryImages = function (response) {
+  response.photos.forEach((image) => {
+    const galleryItemContainer = document.createElement("figure");
+    galleryItemContainer.className = "gallery-item-container";
+    galleryItemContainer.className = `item${itemNum}`;
+
+    const galleryItemLink = document.createElement("a");
+    galleryItemLink.className = "gallery-item-link";
+    galleryItemLink.href = image.url;
+    galleryItemLink.target = "_blank";
+
+    const galleryItem = document.createElement("div");
+    galleryItem.className = "gallery-item";
+    galleryItem.title = image.alt;
+    galleryItem.setAttribute(
+      "style",
+      `background-image: url(${image.src.large2x})`
+    );
+
+    galleryItemLink.append(galleryItem);
+    galleryItemContainer.append(galleryItemLink);
+    photoGalleryEl.append(galleryItemContainer);
+
+    itemNum++;
+  });
+};
